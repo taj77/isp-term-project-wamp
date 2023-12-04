fbbd27fba0db48f41fd1ef5f1f110cba66e594f2
taj77 <122384998+taj77@users.noreply.github.com>
2023-12-04 17:08:00 -0500

    add other dependencies

---
+++ HomeTeamCreativity-prod/react/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 80,
+    proxy: {
+      "/api": "http://api:80",
+    },
+    strictPort: true,
+    hmr: {
+      clientPort: 80,
+    },
+  },
+});
